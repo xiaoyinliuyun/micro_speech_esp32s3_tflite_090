49653d19aeff2c4c6a1d9dc2e57d84e377478f9b
yangkunjian <letgo2048@163.com>
2023-07-13 22:50:01 +0800

    finish

--- src/command_responder.cpp
+++ src/command_responder.cpp
@@ -78,10 +78,10 @@ void RespondToCommand(tflite::ErrorReporter *error_reporter,
                       uint8_t score, bool is_new_command)
 {
   static int32_t last_timestamp = 0;
-  // if (score < 150)
-  // {
-  //   return;
-  // }
+  if (score < 80)
+  {
+    return;
+  }
   // Score List Update
   uint8_t command = COMMAND_SILENCE;
   memset(scoreList, 0, sizeof(scoreList));
